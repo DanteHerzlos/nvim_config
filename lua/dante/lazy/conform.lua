0317ebab38e3b0c63af1e27667b8870a95a30854
DanteHerzlos <streat13@gmail.com>
2025-08-15 01:00:14 +0300

    feat: add peek and supermaven

--- lua/dante/lazy/conform.lua
+++ lua/dante/lazy/conform.lua
@@ -14,13 +14,25 @@ return {
 	opts = function()
 		local plugin = require("lazy.core.config").plugins["conform.nvim"]
 		local opts = {
+			default_format_opts = {
+				timeout_ms = 3000,
+				async = false, -- not recommended to change
+				quiet = false, -- not recommended to change
+				lsp_format = "fallback", -- not recommended to change
+			},
 			format_on_save = {
-				lsp_format = "fallback",
+				lsp_format ="prettier",
 				timeout_ms = 500,
 			},
 			formatters_by_ft = {
 				lua = { "stylua", lsp_format = "fallback" },
-				javascript = { "prettier", lsp_format = "fallback" },
+        json = { lsp_format = "prettier" },
+				javascript = { lsp_format = "fallback" },
+				typescript = { lsp_format = "fallback" },
+        typescriptreact = { lsp_format = "fallback" },
+        javascriptreact = { lsp_format = "fallback" },
+        css = { lsp_format = "prettier" },
+				python = { lsp_format = "fallback" },
 			},
 		}
 		return opts
